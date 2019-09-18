6f6925e29914c87ce81c7ce73660fbbfea849729
Hampus Nilsson <mail@hjnilsson.com>
2019-09-18 22:38:38 +0200

    Trying to debug schedule

--- functions/src/schedule-generator/speakers-sessions-schedule-map.js
+++ functions/src/schedule-generator/speakers-sessions-schedule-map.js
@@ -23,6 +23,9 @@ function sessionsSpeakersScheduleMap(sessionsRaw, speakersRaw, scheduleRaw) {
                 const subSessionsLen = timeslot.sessions[sessionIndex].items.length;
                 for (let subSessionIndex = 0; subSessionIndex < subSessionsLen; subSessionIndex++) {
                     const sessionId = timeslot.sessions[sessionIndex].items[subSessionIndex];
+                    if (sessionsRaw.hasOwnProperty(sessionId)) {
+                        console.error(`Unknown session ${sessionId}`)
+                    }
                     const subsession = sessionsRaw[sessionId];
                     const mainTag = subsession.tags ? subsession.tags[0] : 'General';
                     const endTimeRaw = timeslot.sessions[sessionIndex].extend
